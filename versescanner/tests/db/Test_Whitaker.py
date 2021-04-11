900c5f8df9c580758d76824388cb028f822c2124
benoit.lagae <benoit.lagae@hotmail.com>
2021-04-11 23:09:21 +0200

    Test: put all words in db through Whitaker's Words

---
+++ versescanner/tests/db/Test_Whitaker.py
@@ -0,0 +1,20 @@
+import unittest
+from whitakers_words.parser import Parser
+from versescanner.util.utils import set_django
+
+set_django()
+
+from versescanner.models.metadata import DatabaseVerse
+
+
+class TestWhitaker(unittest.TestCase):
+
+    def test_whitaker_all(self):
+        parser = Parser()
+        verses = DatabaseVerse.objects.all()
+        for verse in verses:
+            for word in verse.contents.split():
+                try:
+                    floo = ''.join(x for x in word if x.isalpha())
+                    if floo:
+                        parser.parse(floo)
