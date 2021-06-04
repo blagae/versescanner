818c3f0c725daa1d790bf334b83ed44f50171510
benoit.lagae <benoit.lagae@hotmail.com>
2021-06-04 20:02:14 +0200

    Don't do VAVC for fieri (if DatabaseBridge)

--- versescanner/tests/db/Test_Whitaker.py
+++ versescanner/tests/db/Test_Whitaker.py
@@ -19,16 +19,28 @@ def wat(a, b):
 class TestWhitaker(unittest.TestCase):
 
     def test_whitaker_all(self):
+        """ this method is for stupid exploration testing """
         parser = Parser()
-        dicc = {}
+        dicc = dict()
+        quoo = dict()
+        i = 0
         # verses = Verse.objects.filter(id__lte=500)
         verses = Verse.objects.all()
         for verse in verses:
+            croo = set()
             for word in verse.contents.split():
                 floo = ''.join(x for x in word if x.isalpha())
                 if floo and floo not in dicc:
                     result = parser.parse(floo)
                     dicc[floo] = 0
                     for form in result.forms:
+                        if form.text not in quoo:
+                            txts = {analysis.lexeme for analysis in form.analyses.values()}
+                            quoo[form.text] = txts
+                            for x in txts:
+                                if x.id and x.id in croo:
+                                    i += 1
+                                croo.add(x.id)
                         dicc[floo] += len(form.analyses)
-        
+        shmoo = {k:v for k, v in quoo.items() if len(v) > 1}
+        return dicc
